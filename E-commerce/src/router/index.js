a289931bbd6329437c14f231e7f166457e773e90
SAM-Khema <kemasam2001@gmail.com>
2023-07-08 16:49:37 +0700

    change route

--- E-commerce/src/router/index.js
+++ E-commerce/src/router/index.js
@@ -5,12 +5,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/guest',
+      path: '/',
       name: 'guest',
       component: () => import('../views/guest.vue')
     },
